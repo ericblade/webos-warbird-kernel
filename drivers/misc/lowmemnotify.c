ede85dbdece3901a1168a4d43124a8ecdf5ab0d7
uNiXpSyChO <marco@unixpsycho.com>
2012-01-01 20:26:59 -0500

    TMC hack

--- drivers/misc/lowmemnotify.c
+++ drivers/misc/lowmemnotify.c
@@ -190,7 +190,7 @@ unsigned long memnotify_get_free(void)
 	if (other_free > totalreserve_pages)
 		free += other_free - totalreserve_pages;
 
-	return free;
+	return totalram_pages;
 }
 EXPORT_SYMBOL(memnotify_get_free);
 
@@ -210,7 +210,7 @@ unsigned long memnotify_get_used(void)
 	used_swap = total_swap_pages - nr_swap_pages;
 	used_mem = totalram_pages - free_mem;
 
-	return used_mem + used_swap;
+	return used_swap;
 }
 
 /** 
